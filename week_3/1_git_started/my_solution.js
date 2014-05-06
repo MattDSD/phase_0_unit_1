ccc67cd7744d9ef80a793e53ba73a4a27ea5647c
MattDSD <mgd2233@gmail.com>
2014-05-06 02:11:38 -0700

    update_my_solution.js

--- week_3/1_git_started/my_solution.js
+++ week_3/1_git_started/my_solution.js
@@ -2,8 +2,8 @@
 Define the following commands and describe how they work/what they do.  
 
 
-#### add
-<!-- Your defnition here -->
+git add
+This command allows you to add files to your repo. 
 
 #### branch
 <!-- Your defnition here -->
@@ -14,8 +14,9 @@ Define the following commands and describe how they work/what they do.
 #### clone
 <!-- Your defnition here -->
 
-#### commit
+git commit -m "Leave note about changes"
 <!-- Your defnition here -->
+This command allows you to upload and changes to your repo and leave notes about the changes you made. 
 
 #### fetch
 <!-- Your defnition here -->
@@ -38,7 +39,9 @@ Define the following commands and describe how they work/what they do.
 #### rm
 <!-- Your defnition here -->
 
-#### status
+git status
+This command gives you the status of the current directory that you are in.  Have the files recently been committed, what 
+files were recently added, most recent commit notes, etc. 
 
 
 ## Release 4: Git Workflow
@@ -53,4 +56,4 @@ Define the following commands and describe how they work/what they do.
 //Initialize a Repo- Git init (short for initialize)
 //Commit changes - git add (file name or --all)
 //Commit changes - git commit -m "note" 
-//Check repo statis- git status 
+//Check repo status- git status 
