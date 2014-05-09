b4dc9897ca1dee642e7ecd0f4660f02496b82f50
MattDSD <mgd2233@gmail.com>
2014-05-09 00:30:05 -0700

    Final_my_solution.js

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -113,12 +113,29 @@ function taunt(str){
   this.rage = true;
 } 
 
-// Reflection
+// Reflection: 
+// I worked on this project for several hours with Arik Gadye.  We tried to create a storyline that would be fun like the 
+// Code Combat games and then we defined all necessary objects that would need to be called throughout the game.   
+// This exercise was both challenging and enjoyable.  We are pretty unsure of how this code works, so we will need to revisit this 
+//code after we learn a little bit more about JS. 
+// 
+// What parts of your strategy worked? What problems did you face? We had a great time writing the psedocode, the storyline.  We 
+//also had a great time creating the objects and functions.  We are unforotunately not sure how to run the code, so we are 
+//not sure if all of our work was good/passable. 
+
+//What questions did you have while coding? What resources did you find to help you answer them?
+//We had a few questions regarding how to create objects.  We had trouble determining whether or not the code would run.
+
+
+//What concepts are you having trouble with, or did you just figure something out? If so, what? I am still struggling with 
+//creating test code and running it through the terminal window. I am also still learning how to use git commands in terminal. 
+//Did you learn any new skills or tricks? I learned better practices to pair program.  It is starting to get more comfortable. 
+
+//How confident are you with each of the Learning Competencies? 
+//Which parts of the challenge did you enjoy? I loved building storylines/pseudocode.  I also enjoy building the objects and functions for each part. 
+//Which parts of the challenge did you find tedious? Nothing was really tedious, but it took a few minutes to ensure that each of our functions were 
+//working properly. 
 // 
 // 
 // 
-// 
-// 
-// 
-// 
-// 
+//
