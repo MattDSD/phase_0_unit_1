cc979ba0daec0e5610abe1317f7706455da3c52e
MattDSD <mgd2233@gmail.com>
2014-05-09 12:55:55 -0700

    refactor_final.js

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -113,6 +113,115 @@ function taunt(str){
   this.rage = true;
 } 
 
+//Refactored Code 
+
+// The STORY:
+//(The story is set on a XY axis with no boundary restrictions) 
+//Magnusson and the evil ogre Loki are out to get one another.  Both Loki and Magnusson begin the game in a 
+//state of aggression (aggressive: true).  'aggression: true' equates to each character moving faster  during the 
+//game. Each character has the ability to talk to one another and taunt one another.  If a knight.says or ogre.says, 
+//conversation will proceed.  If knight.taunts or ogre.taunts, the recipient of the taunt will become enraged (rage: true).
+//In our game, Rage: true will instantly cause the enraged character to attack the other.  
+//
+//Characters can befriend one another, in lieu of the holiday season. befriend: changes aggressive to false.  In 
+//order to befriend another character, or calm their rage down, call Magnusson.befriend(Loki) or Loki.befriend(Magnusson).
+//Each character has a weight, height, name, rage, aggression and vector position values.  To change pos: Vector XY,
+//you can call knight or ogre.moveXY(xy coordinates). The last property that we created is attackXY, which allows
+//each character to attack one another if they are within 10 units of one another.  If one of the characters is hit
+//alive goes from true to false.
+//
+//The games is in your hands.
+//
+var knight = {
+  x: 0,
+  y: 0,
+  arrow: {
+     x: 0,
+     y: 0,
+     speed: 100
+	},
+  aggresive: true,
+  rage: false,
+  alive: true,
+  name: "Magnusson",
+  height: 8,
+  weight: 100,
+  moveXY: function(x, y) {
+  	this.x = x;
+  	this.y = y;
+  },
+  says: function(str) {
+  	console.log(str);
+  },
+  attackXY: function (x, y){
+	this.arrow.x = x;
+	this.arrow.y = y;
+	console.log("Arrow hit at ".concat(this.arrow.x).concat(", ").concat(this.arrow.y));
+	},
+	taunt: function(str){
+	console.log(str);
+	this.rage = true;
+	},
+	befriend: function(name){
+	 this.agressive = false;
+	 console.log("Lets be friends, ".concat(name));
+	},
+	betray: function(name){
+		this.rage = true;
+		this.attackXY();
+	} 
+  };
+  
+  knight.says("Hello!");
+  knight.taunt("Die!");
+  console.log(knight.rage);
+  knight.attackXY(55, 33);
+  knight.moveXY(40, 50);
+  knight.betray();
+  knight.befriend("Loki");
+  
+var ogre = {
+   name: "Loki",   
+   height: 11, 
+   weight: 200, 
+   alive: true,
+   x: 0,
+   y: 0,
+   axe: {
+     x: 0,
+     y: 0
+   },
+   moveXY: function(x, y) {
+   	this.x = x;
+   	this.y = y;
+   },
+   taunt: function(str) {
+   	console.log(str);
+   	this.rage = true;
+   },
+   says: function(str){
+     console.log(str);
+   },
+   attackXY: function(x, y){
+     this.axe.x = x;
+     this.axe.y = y;
+     console.log("Axe swung at ".concat(this.axe.x).concat(", ").concat(this.axe.y));
+   },
+   betray: function(name){
+    this.rage = true;
+	  this.attackXY();
+   },
+  agressive: true,
+  rage: false
+}; 
+
+ogre.says("Hello!");
+ogre.taunt("I'll eat you!");
+console.log(ogre.rage);
+ogre.attackXY(33, 21);
+ogre.betray("Magnusson");
+//ogre.befriend();
+
 // Reflection: 
 // I worked on this project for several hours with Arik Gadye.  We tried to create a storyline that would be fun like the 
 // Code Combat games and then we defined all necessary objects that would need to be called throughout the game.   
